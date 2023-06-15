1b7bd2d49e7e9cc2a9f23bf8935f427435c92386
Sam Jusaitis <hello@samjusaitis.com>
2023-06-15 19:40:14 +0930

    Fix useBottomSheetSpringConfigs re-rendering bug

--- src/hooks/useBottomSheetSpringConfigs.ts
+++ src/hooks/useBottomSheetSpringConfigs.ts
@@ -8,5 +8,23 @@ import type { WithSpringConfig } from 'react-native-reanimated';
 export const useBottomSheetSpringConfigs = (
   configs: Omit<WithSpringConfig, 'velocity'>
 ) => {
-  return useMemo(() => configs, [configs]);
+  return useMemo(() => {
+    const _configs: Omit<WithSpringConfig, 'velocity'> = {
+      damping: configs.damping,
+      mass: configs.mass,
+      stiffness: configs.stiffness,
+      overshootClamping: configs.overshootClamping,
+      restSpeedThreshold: configs.restSpeedThreshold,
+      restDisplacementThreshold: configs.restDisplacementThreshold,
+    };
+
+    return _configs;
+  }, [
+    configs.damping,
+    configs.mass,
+    configs.stiffness,
+    configs.overshootClamping,
+    configs.restSpeedThreshold,
+    configs.restDisplacementThreshold,
+  ]);
 };
